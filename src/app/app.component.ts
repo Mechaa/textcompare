1da32f2c015227340ffb4c3074a5a8bc80dca583
Mechaa <dkam@sol.dk>
2020-02-19 20:21:30 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-
+import * as JsDiff from 'diff';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  textarea: any;textarea2: any;textarea3: any;
-compare(value:string ,secondValue:string ,thirdValue:string ):void{
-  console.log(value + ' ' + secondValue +' ' + thirdValue)
-  //(value: string, secondValue: string ,thirdValue:string)
+  textarea: string; textarea2: string;textarea3: string;
+//Button Press
+compare():void{
+  console.log(this.textarea + this.textarea2 + this.textarea3)
+}
+
+compareValues(value: string, value2: string): void{
+
 }
 }
